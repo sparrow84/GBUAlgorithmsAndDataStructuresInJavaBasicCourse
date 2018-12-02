c0387faa1781c0acdd5b466b464e65ec70923ad4
sparrow84 <vorobey280184@mail.ru>
2018-12-02 20:17:42 +0700

    MyArrayDeque insertLeft - in progress

--- src/main/java/lesson03/MyArrayDeque.java
+++ src/main/java/lesson03/MyArrayDeque.java
@@ -9,6 +9,8 @@ RemoveRight
 
 public class MyArrayDeque<Item> {
 
+    private static final int SIZE_INCREASE_RATIO = 2;
+
     private Object[] queue;
 
     private int start;
@@ -30,12 +32,55 @@ public class MyArrayDeque<Item> {
     }
 
     // метод проверяет очередь на заполненность
-    private boolean isEmpty() {
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public void insertLeft () {
-        //
+    public void resize () {
+        Object[] temp = new Object[size * SIZE_INCREASE_RATIO];
+
+
+        for (int i = 0; i < size; i++) {
+            temp[i] = queue[(start + i) % size];
+        }
+
+
+
+        queue = temp;
+        start = 0;
+        end = size;
+    }
+
+    public void resize1 (int capacity) {
+
+        Object[] temp = new Object[capacity];
+
+        for (int i = 0; i < size; i++)
+            temp[i] = queue[(start + i) % size];
+
+        queue = temp;
+        start = 0;
+        end = size;
+    }
+
+    public void insertLeft (Item item) {
+        if (queue.length == size)
+            resize();
+
+        if (start > 0)
+            start--;
+        else
+            start = queue.length - 1;
+
+        queue[start] = item;
+        size++;
+
+
+        //FIXME
+        if ()
+
+
+
     }
 
     public void inserRight () {
