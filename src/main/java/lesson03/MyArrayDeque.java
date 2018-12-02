ec9d9e9131261d56028515d509c59d20858e3ea8
sparrow84 <vorobey280184@mail.ru>
2018-12-02 19:29:53 +0700

    MyArrayDeque

--- src/main/java/lesson03/MyArrayDeque.java
+++ src/main/java/lesson03/MyArrayDeque.java
@@ -1,4 +1,74 @@
 package lesson03;
 
-public class MyArrayDeque {
+/*
+InsertLeft
+InserRight
+RemoveLeft
+RemoveRight
+ */
+
+public class MyArrayDeque<Item> {
+
+    private Object[] queue;
+
+    private int start;
+
+    private int end;
+
+    private int size;
+
+    public MyArrayDeque() {
+        start = 0;
+        end = 0;
+        size = 0;
+        queue = new Object[1];
+    }
+
+    // метод возвращает размер очереди
+    public int getSize() {
+        return size;
+    }
+
+    // метод проверяет очередь на заполненность
+    private boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void insertLeft () {
+        //
+    }
+
+    public void inserRight () {
+        //
+    }
+
+    public void removeLeft () {
+        //
+    }
+
+    public void removeRight () {
+        //
+    }
+
+//    public void  () {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
