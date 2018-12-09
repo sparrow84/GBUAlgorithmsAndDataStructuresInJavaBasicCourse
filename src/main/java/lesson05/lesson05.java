35e1b70b9941a23edeef2b9232d0d650c1bdf81d
sparrow84 <vorobey280184@mail.ru>
2018-12-09 17:52:44 +0700

    Lesson05 hanoiTower cyclic in progress

--- src/main/java/lesson05/lesson05.java
+++ src/main/java/lesson05/lesson05.java
@@ -5,53 +5,69 @@ import java.util.Stack;
 public class lesson05 {
     public static void main(String[] args) {
 
-        System.out.println(recursiveExponentiation(2, 62));
+        double number = 5;
+        int degree = -2;
+
+        System.out.println(recursiveExponentiation(number, degree));
+        System.out.println(cyclicExponentiation(number,degree));
+
 
         Stack<Integer> red = new Stack<>();
-        Stack<Integer> geen = new Stack<>();
+        Stack<Integer> green = new Stack<>();
         Stack<Integer> black = new Stack<>();
 
-        for(int i = 1; i < 9; i++)
+        int elementsCount = 8;
+
+        for(int i = elementsCount; i > 0; i--)
             red.push(i);
 
-        hanoiTower(red,geen,black);
+//        hanoiTowerR(elementsCount, red,green,black);
 
     }
 
-    public static void hanoiTower(Stack<Integer> full, Stack<Integer> temp, Stack<Integer> empty) {
-
-        int elementsCount = full.size();
-
-        if(!(empty.size() == elementsCount)) {
-
-
-
-
-
-        }
-
+    public static void hanoiTowerC(int elementsCount, Stack<Integer> from, Stack<Integer> to, Stack<Integer> temp) {
 
     }
 
-    public static long recursiveExponentiation(long number, long degree) {
+    public static void hanoiTowerR(int elementsCount, Stack<Integer> from, Stack<Integer> to, Stack<Integer> temp) {
+        if(elementsCount > 0) {
+            hanoiTowerR(elementsCount - 1, from, temp, to);
+            to.push(from.pop());
 
-        long res = 0;
+            System.out.println("\n\n\n\n\n");
+            System.out.println(from);
+            System.out.println(to);
+            System.out.println(temp);
 
-        if(degree > -1) {
-            if (degree == 0)
-                res = 1;
-            else if (degree == 1)
-                res = number;
-            else
-                res = number * recursiveExponentiation(number, degree - 1);
-
-        } else {
-            try {
-                throw new Exception();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            hanoiTowerR(elementsCount -1, temp, to, from);
         }
+    }
+
+    public static double recursiveExponentiation(double number, int degree) {
+        double res = 0;
+
+        if (degree == 0)
+            res = 1;
+        else
+            res = number * recursiveExponentiation(number, Math.abs(degree) - 1);
+
+        if (degree < 0)
+            res = 1 / res;
+
+        return res;
+    }
+
+    public static double cyclicExponentiation(double number, int degree) {
+        double res = number;
+
+        if(Math.abs(degree) == 0)
+            res = 1;
+        else
+            for (int i = 1; i < Math.abs(degree); i++)
+                res = res * number;
+
+        if(degree < 0)
+            res = 1 / res;
 
         return res;
     }
